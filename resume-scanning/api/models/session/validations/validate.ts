5d72a86e4eb99ee5d9e21b584e8251563d694d8c
Kelvin Mock <kelvin.mock@gmail.com>
2025-01-18 13:51:50 -0500

    Create validate.ts

---
+++ resume-scanning/api/models/session/validations/validate.ts
@@ -0,0 +1,10 @@
+import type { SessionJobRolesFieldValidationContext } from "gadget-server";
+
+/**
+ * Validation code for field JobRoles on session
+ */
+export default async ({api, record, errors, logger, field}: SessionJobRolesFieldValidationContext) => {
+  // TODO: implement validation
+  // access things like `record.JobRoles` to get the fields's data
+  // add to the `errors` with `errors.add('JobRoles', "did not pass the validation")`
+};
